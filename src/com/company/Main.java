ebb1cf3b21de6a2d73bc9814bbff6758463bc70d
ShahnozaSh <shahnoza_3010@mai.ru>
2020-10-29 18:01:20 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 1; i < 5; i++) {
+            Runner runner = new Runner("Runner:"+ i);
+            runner.setNextRunner(i+1);
+            runner.index=0;
+            runner.start();
+            runner.join();
+
+        }
+        for (int i = 5; i>1; i++) {
+            Runner runner = new Runner("Runner :"+1);
+            runner.setNextRunner(i-1);
+            runner.start();
+            runner.join();
+
+        }
+        System.out.println("Runner 1 берет палочку");
+
+    }
+}
